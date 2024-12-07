bcb278f9773c809b603b262a33d9e95c6d933553
Pham Nguyen Khoa <khoapnse183214@fpt.edu.vn>
2024-12-07 11:22:29 +0700

    Kết nối database

--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+import * as database from "../Artical/config/database"
+import Articel from "./models/articel.model";
+
+database.connect();
+
 
 const app: Express = express();
-const port: number = 3000;
+const port: number | string = process.env.PORT || 3000;
+
+
 
 // Rest API
-app.get("/articles", (req: Request, res: Response) => {
+app.get("/articles", async (req: Request, res: Response) => {
+    const articels = await Articel.find({
+        deleted: false
+    })
+    
     res.json({
-        articles: []
+        articles: articels
     });
 });
 

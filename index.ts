ad0e91d1f432e3f75cdd36f88e82c965378947a5
Pham Nguyen Khoa <khoapnse183214@fpt.edu.vn>
2024-12-07 12:08:43 +0700

    Config graphQL

--- index.ts
+++ index.ts
@@ -1,29 +1,36 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
+import * as database from "../Artical/config/database";
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { typeDefs } from "./typeDefs";
+import { resolvers } from "./resolvers";
 
-dotenv.config();
-import * as database from "../Artical/config/database"
-import Articel from "./models/articel.model";
+const startServer = async () => {
+  dotenv.config();
 
-database.connect();
+  database.connect();
 
+  const app: Express = express();
+  const port: number | string = process.env.PORT || 3000;
 
-const app: Express = express();
-const port: number | string = process.env.PORT || 3000;
+  // Add JSON middleware
+  app.use(express.json());
 
+  // GraphQL
 
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-// Rest API
-app.get("/articles", async (req: Request, res: Response) => {
-    const articels = await Articel.find({
-        deleted: false
-    })
-    
-    res.json({
-        articles: articels
-    });
-});
+  await apolloServer.start();
 
-app.listen(port, () => {
+  app.use('/graphql', expressMiddleware(apolloServer));
+
+  app.listen(port, () => {
     console.log(`App listening on port ${port}`);
-});
+  });
+};
+
+startServer();
